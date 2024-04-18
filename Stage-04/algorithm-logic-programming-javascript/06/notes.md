88c3cf718953067ba60be6342498b55012ffc5c1
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-18 18:17:34 -0300

    feat: add guessing game exercise

---
+++ Stage-04/algorithm-logic-programming-javascript/06/notes.md
@@ -0,0 +1,18 @@
+[] Controlar o fluxo da aplicação
+[] Estrutura de repetição
+  [] while
+[] Gerar números aleatório
+[] Math
+  - Pacote com funcionalidades matemáticas
+  - Math.random()
+  - Math.round()
+  - Math.ceil() 
+  - Math.floor()
+[] Tipo de dado: NaN
+  - não é um número
+[] Tipo de erro: SyntaxError
+  - erro de sintaxe;erro de escrita de código
+[] console
+  - Pacote com funcionalidades para o console do devtools (browser)
+[] template literals ou template strings
+  - interpolação de dado
---
+++ Stage-04/algorithm-logic-programming-javascript/06/script.js
@@ -0,0 +1,32 @@
+/*
+ ** Jogo da advinhação **
+
+ Apresente a mensagem ao usuário:
+ "Advinhe o número que estou pensando? Está entre 0 e 10"
+
+ Crie uma lógica para gerar um número aleatório
+ e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
+
+ Enquanto o usuário não advinhar o número, mostrar a mensagem:
+ "Erro, tente novamente:"
+
+ Caso o usuário acerte o número, apresentar a mensagem:
+ "Parabéns! Você advinhou o número em x tentativas"
+
+ Substitua o "x" da mensagem, pelo número de tentativas.
+ */
+
+let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 1
+
+while (Number(result) != randomNumber) {
+  result = prompt("Erro, tente novamente:")
+
+  xAttempts++
+}
+
+xAttempts == 1 
+  ? alert(`Parabéns! Você advinhou o número em ${xAttempts} tentativa`)
+  : alert(`Parabéns! Você advinhou o número em ${xAttempts} tentativas`)
