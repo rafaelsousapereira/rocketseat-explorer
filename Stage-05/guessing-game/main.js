b1510fc428b9eac9495245cf47ec3acfed07a1c0
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-21 17:20:32 -0300

    feat: add try and reset events

--- Stage-05/guessing-game/main.js
+++ Stage-05/guessing-game/main.js
@@ -1,19 +1,36 @@
-const randomNumber = Math.round(Math.random())
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+const randomNumber = Math.round(Math.random() * 10)
 let xAttenpts = 1
 
-function handleClick(event) {
+// funções callback
+function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
 
-    document
-      .querySelector(".screen2 h2")
-      .innerText = `Acertou em ${xAttenpts} tentativas!`
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttenpts} tentativas!`
   }
 
+  inputNumber.value = ""
   xAttenpts++
 }
+
+function handleResetClick() {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+
+  xAttenpts = 1
+}
+
+// Eventos
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
