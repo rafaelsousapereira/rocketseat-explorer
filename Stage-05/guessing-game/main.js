cae383f6eeab158100a4e6009c8539e818fb51ea
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-23 20:25:52 -0300

    refactor: add refactoring and more features

--- Stage-05/guessing-game/main.js
+++ Stage-05/guessing-game/main.js
@@ -1,36 +1,62 @@
+// variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttenpts = 1
 
-// funções callback
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleEnterKey)
+
+// funções
 function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
-
+    toggleScreen()
     screen2.querySelector("h2").innerText = `Acertou em ${xAttenpts} tentativas!`
   }
+
+  handleLimitNumbers()
+  
+  handleEmptyValue()
 
   inputNumber.value = ""
   xAttenpts++
 }
 
-function handleResetClick() {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
 
+function handleResetClick() {
+  toggleScreen()
   xAttenpts = 1
+  randomNumber = Math.round(Math.random() * 10)
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
+function handleEnterKey(e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
 
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', handleResetClick)
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+function handleLimitNumbers() {
+  if (inputNumber.value < 0 || inputNumber.value > 10) {
+    alert("O número do jogo deve ser entre 0 e 10")
+  }
+}
+
+function handleEmptyValue() {
+  if (inputNumber.value == "") {
+    alert("Digite um número para jogar")
+  }
+}
