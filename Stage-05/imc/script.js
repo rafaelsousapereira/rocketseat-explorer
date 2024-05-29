7d1a0fde406977d0540f7f7a381004c7e2ff61a6
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-29 10:33:51 -0300

    fix: remove comments

--- Stage-05/imc/script.js
+++ Stage-05/imc/script.js
@@ -3,10 +3,6 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-// const modalWrapper = document.querySelector('.modal-wrapper')
-// const modalMessage = document.querySelector('.modal .title span')
-// const modalBtnClose = document.querySelector('.modal button.close')
-
 const Modal = {
 
   wrapper: document.querySelector('.modal-wrapper'),
