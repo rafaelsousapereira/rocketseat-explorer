91f54b89165c806583ca6d910af7168378e171f0
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-29 09:23:13 -0300

    feat: add logic imc

---
+++ Stage-05/imc/script.js
@@ -0,0 +1,28 @@
+// variáveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+
+form.onsubmit = function(event) {
+  event.preventDefault()
+  
+  const weight = inputWeight.value
+  const height = inputHeight.value
+  
+  const result = IMC(weight, height)
+  const message = `Seu IMC é ${result}`
+
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = () => modalWrapper.classList.remove('open')
+
+const IMC = (weight, height) => {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
+}
