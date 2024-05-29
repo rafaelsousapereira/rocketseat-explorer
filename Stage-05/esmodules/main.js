b2dbfad5d248d96b229543158b46c5990d897447
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-29 09:57:39 -0300

    feat: add ES6 Modules export default

---
+++ Stage-05/esmodules/main.js
@@ -0,0 +1,3 @@
+import qualquerNome from './utils.js' // nessa linha ele importa e já executa o arquivo
+
+console.log(qualquerNome)
---
+++ Stage-05/esmodules/utils.js
@@ -0,0 +1,3 @@
+const message = "mensagem" // essa variável fica isolada nesse arquivo, e não consigo usar ela em outro lugar
+
+export default message
