360e066eb6443fa9027db5bb55090a18199d3803
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-29 10:31:15 -0300

    feat: add ES6 Modules import * as

--- Stage-05/esmodules/main.js
+++ Stage-05/esmodules/main.js
@@ -1,3 +1,12 @@
-import { sum as soma } from './utils.js'
+import * as calculator from './utils.js'
 
-console.log(soma(20, 34))
+console.log(calculator.sum(10, 10))
+console.log(calculator.divide(40, 4))
+console.log(calculator.multiply(5, 5))
+console.log(calculator.subtract(12, 2))
+
+/*
+  Referência:
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
+*/
--- Stage-05/esmodules/utils.js
+++ Stage-05/esmodules/utils.js
@@ -1,3 +1,7 @@
-export function sum(a, b) {
-  return a + b
-}
+export const sum = (a, b) => a + b
+export const multiply = (a, b) => a * b
+export const subtract = (a, b) => a - b
+export const divide = (a, b) => a / b
+
+// ou
+// export { sum, multiply, subtract, divide }
