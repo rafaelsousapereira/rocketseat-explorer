b31a22c6a02f289f1d37b78caa22edf2e94c5d5f
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-30 14:19:20 -0300

    feat: add modules in the modal

--- Stage-05/imc/script.js
+++ Stage-05/imc/script.js
@@ -1,21 +1,9 @@
+import { Modal } from './modal.js'
+
 // variáveis - variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const Modal = {
-
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-
-  open: function() {
-    Modal.wrapper.classList.add('open')
-  },
-  close: function() {
-    Modal.wrapper.classList.remove('open')
-  }
-}
 
 form.onsubmit = function(event) {
   event.preventDefault()
@@ -29,8 +17,6 @@ form.onsubmit = function(event) {
   Modal.message.innerText = message
   Modal.open()
 }
-
-Modal.buttonClose.onclick = () => Modal.close()
 
 const IMC = (weight, height) => {
   return (weight / ((height / 100) ** 2)).toFixed(2)
